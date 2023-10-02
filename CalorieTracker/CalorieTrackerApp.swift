1dfc4206e9628b4cc6d34d9daccae47ccbb42f80
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2023-10-01 22:49:09 -0700

    First commit

---
+++ CalorieTracker/CalorieTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieTrackerApp.swift
+//  CalorieTracker
+//
+//  Created by SHAdON . on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorieTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
